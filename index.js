f9fcba1ded9bcda0ab805ac19635a6a3a4041c1a
Matilde Ribeiro <53408372+tilda21@users.noreply.github.com>
2023-04-08 21:18:17 +0100

    structure app for heroku

--- index.js
+++ index.js
@@ -40,3 +40,13 @@ mongoose
   .catch((error) => {
     console.error(error.message);
   });
+
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
